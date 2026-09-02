e859af1b4052b0d3f065cda0ab016b97ae931bc7
Tiago augusto Gaido Grizzo <tiagoggrizzo@gmail.com>
2026-09-02 09:26:51 -0300

    Finalizando back + tarefa de fazer as páginas do front

--- ToDo/Backend/index.js
+++ ToDo/Backend/index.js
@@ -7,18 +7,19 @@ import swaggerUi from "swagger-ui-express";
 import { createRequire } from "module";
 import cookieParser from "cookie-parser";
 const PORT = process.env.PORT || 5000;
-const FRONTEND_URL = process.env.FRONTEND_URL ||  "http://localhost:5173";
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 //suporte para importar arquivos json usando ESModules
 const require = createRequire(import.meta.url);
 const swaggerDocument = require("./swagger-output.json");
 const app = new express();
 //comunicação entre front e back usar json
 app.use(express.json());
-app.use(cookieParser());
+
 app.use(cors({
-    credential: true,
+    credentials: true, 
     origin: FRONTEND_URL
 }));
+app.use(cookieParser());
 //obrigatoriamente o swagger deve vir antes das rotas
 app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/ToDo", routesTarefa);
